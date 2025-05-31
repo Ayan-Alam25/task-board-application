2d50b6e373eae3e2c24432212309cb0d10040425
Ayan Alam <ayanalam0025@gmail.com>
2025-05-31 15:52:59 +0530

    Required packages and types are added

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='text-3xl flex  justify-center align-center'>Task Board</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/types/types.ts
@@ -0,0 +1,33 @@
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+}
+
+export type Priority = "high" | "medium" | "low";
+
+export interface Card {
+    id: string;
+    title: string;
+    description: string;
+    createdAt: Date;
+    createdBy: User;
+    priority: Priority;
+    dueDate: Date;
+    assignedTo: User;
+}
+
+export interface Column {
+    id: string;
+    title: string;
+    cards: Card[];
+}
+
+export interface Board {
+    id: string;
+    title: string;
+    description: string;
+    createdAt: Date;
+    createdBy: User;
+    Columns: Column[];
+}
